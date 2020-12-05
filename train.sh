f77694413e7dbaca15663230c1bf3600fed1adc6
Amit <amit.joshiusa@gmail.com>
2020-12-05 17:22:50 -0600

    added train.sh file

---
+++ train.sh
@@ -0,0 +1,1 @@
+python3 main.py     --use_gpu     --model "baseline"     --model_path "baseline_small_squad.pt"     --train_path "datasets/squad_train.jsonl.gz"     --dev_path "datasets/squad_dev.jsonl.gz"     --output_path "baseline_small_squad_preds.txt"     --hidden_dim 128     --bidirectional     --do_test
